9ac90448c1c83931d5a84e2c983f4e6c8ac7041b
Harshit Goel <harshitgoel885@gmail.com>
2025-07-18 16:03:05 +0530

    feat: implement product model with schema and pre-save hook for final price calculation

---
+++ server/src/models/product.controller.js
@@ -0,0 +1,89 @@
+import mongoose from "mongoose";
+
+
+const productSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        description:{
+            type: String,
+            required: true,
+            trim: true,
+        },
+        price:{
+            type: Number,
+            required: true,
+            min: 0,
+        },
+        discount:{
+            type: Number,
+            default: 0
+        },
+        finalPrice:{
+            type: Number
+        },
+        stock:{
+            type: Number,
+            required: true
+        },
+        sold:{
+            type: Number,
+            default: 0
+        },
+        images:{
+            type: String,
+            required: true,
+        },
+        brand:{
+            type: String,
+            required: true,
+        },
+        category:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Category",
+            required: true,
+        },
+        ratings:{
+            type: Number,
+            default: 0,
+            min: 0,
+            max: 5
+        },
+        reviews:{
+            type: Number,
+            default: 0
+        },
+        tags: [
+            {
+                type: String,
+                trim: true
+            }
+        ],
+        seller: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: [true, "Seller ID is required"]
+        },
+        isDeleted: {
+            type: Boolean,
+            default: false
+        }
+
+
+    },
+    {
+        timestamps: true,
+
+    }
+)
+
+
+productSchema.pre("save", function(next){
+    this.finalPrice = this.price - ((this.price * this.discount) / 100);
+  next();
+});
+
+export const Product = mongoose.model("Product", productSchema);
