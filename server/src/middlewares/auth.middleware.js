155857332c50e396bb780b7806e13749868217df
Harshit Goel <harshitgoel885@gmail.com>
2025-11-06 00:35:19 +0530

    refactor: clean up whitespace and formatting in auth middleware for improved readability

--- server/src/middlewares/auth.middleware.js
+++ server/src/middlewares/auth.middleware.js
@@ -3,29 +3,23 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { User } from "../models/user.model.js";
 import jwt from "jsonwebtoken";
 
-
-
 export const verifyJWT = asyncHandler(async(req, res, next) => {
    try {
     const token = req.cookies?.accessToken  || req.header("Authorization")?.replace("Bearer","").trim();
- 
     if(!token){
      throw new ApiError(401 , "Unauthorized request")
     }
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
-     
    const user =  await User.findById(decodedToken?._id).select("-password -refreshToken")
    if(!user){
      throw new ApiError(401,"Invalid Access Token")
    }
-
    if(!user.isActive || user.isDeleted){
     throw new ApiError(403, "Your account is inactive or deleted.")
    }
    if(user.role === "seller" && (user.isSellerSuspended || !user.isSellerApproved)){
     throw new ApiError(403, "Seller account not allowed. Contact support")
    }
- 
    req.user = user;
    next()
    } catch (error) {
