2a71ce8ff0dee71f7b81d87ba8a1aee32c3b5c10
Harshit Goel <harshitgoel885@gmail.com>
2026-01-24 21:51:21 +0530

    feat: add Beauty and Grooming category components and integrate into CustomerRoutes

--- client/customer/src/routes/CustomerRoutes.jsx
+++ client/customer/src/routes/CustomerRoutes.jsx
@@ -4,6 +4,7 @@ import Home from "../pages/Home/Home";
 import Categories from "../pages/Categories/Categories";
 import Men from "../pages/Categories/Men";
 import Women from "../pages/Categories/Women";
+import BeautyAndGrooming from "../pages/Categories/BeautyAndGrooming";
 
 const CustomerRoutes = () => {
   return (
@@ -15,6 +16,8 @@ const CustomerRoutes = () => {
           <Route path="/categories" element={<Categories />} />
           <Route path="/categories/men" element={<Men />} />
           <Route path="/categories/women" element={<Women />} />
+          <Route path="/categories/beauty" element={<BeautyAndGrooming />} />
+
 
         </Route>
       </Routes>
